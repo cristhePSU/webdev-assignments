d72a5520c65b3d20da9d1b52952c786352acc5da
Cristian <cristhe@pdx.edu>
2026-06-09 15:23:53 -0700

    Added JavaScript

---
+++ .vscode/Final_Project/index.js
@@ -0,0 +1,17 @@
+const themeButton = document.getElementById("theme-button");
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
+
+const form = document.getElementById("contact-form");
+const response = document.getElementById("form-response");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    response.textContent =
+        "Thank you! Your message has been received.";
+
+    form.reset();
+});
